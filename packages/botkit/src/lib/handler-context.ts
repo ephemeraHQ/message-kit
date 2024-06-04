ea97f9c0be0dc00d3e456c9590752f0008e1a531
fabri <fguespe@gmail.com>
2024-06-04 13:04:22 -0300

    update

--- packages/botkit/src/lib/handler-context.ts
+++ packages/botkit/src/lib/handler-context.ts
@@ -1,6 +1,5 @@
 import { DecodedMessage } from "@xmtp/xmtp-js";
 import { ContentTypeBotMessage } from "../content-types/Bot.js";
-import { ContentTypeSilent } from "../content-types/Silent.js";
 
 export default class HandlerContext {
   message: DecodedMessage;
@@ -19,31 +18,6 @@ export default class HandlerContext {
 
   async textReply(content: any) {
     await this.message.conversation.send(content);
-  }
-  async grant_access() {
-    await this.message.conversation.send(
-      {
-        content: "grant_access",
-        metadata: {
-          access: true,
-          ...this.context,
-        },
-      },
-      {
-        contentType: ContentTypeSilent,
-      },
-    );
-  }
-  async ping() {
-    await this.message.conversation.send(
-      {
-        content: "ping",
-        metadata: { ...this.context },
-      },
-      {
-        contentType: ContentTypeSilent,
-      },
-    );
   }
   async reply(
     message: string,
--- packages/botkit/src/lib/runner.ts
+++ packages/botkit/src/lib/runner.ts
@@ -1,34 +1,88 @@
 import xmtpClient from "./client.js";
 import HandlerContext from "./handler-context.js";
+import { ContentTypeSilent } from "../content-types/Silent.js";
 
 type Handler = (context: HandlerContext) => Promise<void>;
 
-export default async function run(handler: Handler, newBotConfig?: any) {
+export default async function run(
+  handler: Handler,
+  newBotConfig?: any,
+  accessHandler?: (context: HandlerContext) => Promise<boolean>,
+) {
   const client = await xmtpClient(newBotConfig);
-
+  const { address } = client;
   for await (const message of await client.conversations.streamAllMessages()) {
     try {
-      if (message.senderAddress === client.address) {
+      const {
+        senderAddress,
+        content,
+        contentType: { typeId },
+      } = message;
+
+      if (senderAddress === address) {
         // if same address do nothing
         continue;
-      } else if (message.contentType.typeId == "bot") {
+      } else if (typeId == "bot") {
         //if a bot speaks do nothing
         continue;
       }
       const context = new HandlerContext(
         message,
         newBotConfig?.context ?? {},
-        client.address,
+        address,
       );
       if (
-        message.contentType.typeId == "silent" &&
-        message.content?.content === "/ping"
+        typeId == "silent" &&
+        content?.content === "/access" &&
+        accessHandler
       ) {
         //if a bot speaks do nothing
-        context.ping();
-      } else await handler(context);
+        const accept = await accessHandler(context);
+        if (accept) {
+          // add to group
+          grant_access(message, newBotConfig?.context);
+          continue;
+        }
+      } else if (typeId == "silent" && content?.content === "/ping") {
+        //if a bot speaks do nothing
+        ping(message, newBotConfig?.context, accessHandler);
+        continue;
+      }
+
+      await handler(context);
     } catch (e) {
       console.log(`error`, e);
     }
   }
+}
+
+async function grant_access(message: any, context: any) {
+  // add group member
+  await message.conversation.send(
+    {
+      content: "",
+      metadata: {
+        type: "access",
+        ...context,
+      },
+    },
+    {
+      contentType: ContentTypeSilent,
+    },
+  );
+}
+async function ping(message: any, context: any, accessHandler: any) {
+  await message.conversation.send(
+    {
+      content: "",
+      metadata: {
+        type: "ping",
+        access: accessHandler ? true : false,
+        ...context,
+      },
+    },
+    {
+      contentType: ContentTypeSilent,
+    },
+  );
 }
