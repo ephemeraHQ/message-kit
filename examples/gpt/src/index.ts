aa8aca8f935c37c186b6dc8a88de4aad1f4632fa
fabri <fguespe@gmail.com>
2024-06-06 17:55:37 -0300

    update

--- examples/gpt/src/index.ts
+++ examples/gpt/src/index.ts
@@ -2,31 +2,21 @@ import "dotenv/config";
 import { run, HandlerContext } from "@xmtp/botkit";
 import openaiCall from "./lib/gpt.js";
 
-// Initialize an array to store the conversation history
-const systemPrompt =
-  "You are a helpful assistant that lives inside a web3 messaging app. You love blockchain and decentralization and you are quite funny. You often tell crypto jokes.";
-let conversationHistory = [
-  {
-    role: "system",
-    content: systemPrompt,
-  },
-];
-
 run(async (context: HandlerContext) => {
   const { content, typeId, senderAddress } = context.message;
+  const { content: text } = content;
+
+  // Initialize an array to store the conversation history
+  const systemPrompt =
+    "You are a helpful assistant that lives inside a web3 messaging app. You love blockchain and decentralization and you are quite funny. You often tell crypto jokes.";
 
   try {
-    const { reply, messages } = await openaiCall(
-      content,
-      conversationHistory,
-      senderAddress,
-      systemPrompt,
-    );
-    conversationHistory = messages; // Update the conversation history
-    await context.reply(reply as string);
+    let { reply } = await openaiCall(text, senderAddress, systemPrompt);
+
+    await context.textReply(`${reply}`);
   } catch (error) {
     // Handle the error, for example, by sending an error message to the user
-    await context.reply(
+    await context.textReply(
       "Failed to process your request. Please try again later.",
     );
   }
