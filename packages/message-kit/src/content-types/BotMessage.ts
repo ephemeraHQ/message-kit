aef18fe4f26c77367a0c50c03cd7285ef1f49f2a
Ry Racherbaumer <ry@xmtp.com>
2024-06-18 08:49:29 -0700

    WIP

--- packages/message-kit/src/content-types/BotMessage.ts
+++ packages/message-kit/src/content-types/BotMessage.ts
@@ -3,7 +3,6 @@ import {
   ContentTypeId,
   EncodedContent,
 } from "@xmtp/content-type-primitives";
-import { CommandGroup, User } from "../helpers/types";
 
 export const ContentTypeBotMessage = new ContentTypeId({
   authorityId: "xmtp.org",
@@ -12,15 +11,9 @@ export const ContentTypeBotMessage = new ContentTypeId({
   versionMinor: 0,
 });
 
-export type BotMessageMetadata = {
-  users?: User[];
-  commands?: CommandGroup[];
-};
-
 export type BotMessage = {
   receivers: string[];
   content: string;
-  metadata?: BotMessageMetadata;
 };
 
 export class BotMessageCodec implements ContentCodec<BotMessage> {
@@ -29,13 +22,13 @@ export class BotMessageCodec implements ContentCodec<BotMessage> {
   }
 
   encode(message: BotMessage) {
-    const { receivers, content, metadata } = message; // Include receivers in the destructuring
+    const { receivers, content } = message; // Include receivers in the destructuring
 
     return {
       type: this.contentType,
       parameters: {},
       content: new TextEncoder().encode(
-        JSON.stringify({ receivers, content, metadata }), // Include receivers in the JSON string
+        JSON.stringify({ receivers, content }), // Include receivers in the JSON string
       ),
     };
   }
@@ -45,8 +38,8 @@ export class BotMessageCodec implements ContentCodec<BotMessage> {
 
     try {
       const message = JSON.parse(decodedContent) as BotMessage;
-      const { receivers, content, metadata } = message;
-      return { receivers, content, metadata };
+      const { receivers, content } = message;
+      return { receivers, content };
     } catch (e) {
       return {
         receivers: [],
