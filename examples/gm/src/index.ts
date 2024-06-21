504ad61e471eccdd2b4953bf51f42d8687596489
fabri <fguespe@gmail.com>
2024-06-21 11:02:04 -0300

    update

--- examples/gm/src/index.ts
+++ examples/gm/src/index.ts
@@ -4,10 +4,9 @@ import { run, HandlerContext } from "@xmtp/message-kit";
 run(async (context: HandlerContext) => {
   // Get the message and the address from the sender
   const { content, typeId } = context.message;
-
   if (typeId === "text") {
     const { content: text } = content;
-    if (text?.startsWith("/slots") || text?.startsWith("/slots")) {
+    if (text?.startsWith("/game slots")) {
       await context.reply(`https://slot-machine-frame.vercel.app/`);
     } else {
       await context.reply(`gm`);
--- packages/message-kit/src/v2/handlerContext.ts
+++ packages/message-kit/src/v2/handlerContext.ts
@@ -1,5 +1,7 @@
 import { Conversation, DecodedMessage } from "@xmtp/xmtp-js";
 import { MessageAbstractedV2 } from "../helpers/types.js";
+import { CommandGroup, User, MessageAbstracted } from "../helpers/types.js";
+import { extractCommandValues } from "../helpers/commands.js";
 
 export default class HandlerContext {
   message: MessageAbstractedV2;
@@ -11,10 +13,11 @@ export default class HandlerContext {
     message: DecodedMessage,
     clientAddress: string,
   ) {
+    let content = parseCommands(message, commands ?? [], this.members ?? []);
     this.conversation = conversation;
     this.message = {
       id: message.id,
-      content: message.content,
+      content: content,
       senderAddress: message.senderAddress,
       typeId: message.contentType.typeId,
       sent: message.sent,
@@ -25,4 +28,30 @@ export default class HandlerContext {
   async reply(content: any) {
     await this.conversation.send(content);
   }
+}
+
+function parseCommands(
+  message: DecodedMessage,
+  commands: CommandGroup[],
+  members: User[],
+) {
+  let content = message.content;
+  if (message.contentType.sameAs(ContentTypeText)) {
+    if (content?.startsWith("/")) {
+      const extractedValues = extractCommandValues(
+        content,
+        commands ?? [],
+        members ?? [],
+      );
+      content = {
+        content: content,
+        ...extractedValues,
+      };
+    } else {
+      content = {
+        content: content,
+      };
+    }
+  }
+  return content;
 }
