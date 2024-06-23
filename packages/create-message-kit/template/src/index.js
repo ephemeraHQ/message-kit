d6e0656b09f87c2bdc7c0f5651936bd91e0c22ed
fabri <fguespe@gmail.com>
2024-06-23 16:16:07 -0300

    update

--- packages/create-message-kit/template/src/index.js
+++ packages/create-message-kit/template/src/index.js
@@ -1,9 +1,9 @@
 import "dotenv/config";
 import { run } from "message-kit";
 run(async (context) => {
-    const { content, sender } = context.message;
-    /* Your logic*/
-    //Send the message
-    await context.reply("gm");
+  const { content, sender } = context.message;
+  /* Your logic*/
+  //Send the message
+  await context.reply("gm");
 });
 //# sourceMappingURL=index.js.map
