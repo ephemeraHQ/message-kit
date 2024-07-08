5069403e9e4e05b0fdcffd0e9766b885b97859c3
fabri <fguespe@gmail.com>
2024-07-08 22:49:37 +0200

    update

--- packages/message-kit/src/tests/commands/usernames.ts
+++ packages/message-kit/src/tests/commands/usernames.ts
@@ -10,6 +10,14 @@ export const fakeUsers: User[] = [
     installationIds: [],
   },
   {
+    username: "eva",
+    address: "0xeAc10D864802fCcfe897E3957776634D1AE006B2",
+    inboxId: "3942e354e8cce5e9d550cf152749a795514adbe1c27d97b88b9fcc63a596c7e4",
+    accountAddresses: ["0xeAc10D864802fCcfe897E3957776634D1AE006B2"],
+    installationIds: [],
+    fake: true,
+  },
+  {
     username: "bo",
     address: "0x388547F774377C6BEA0844A3221B0Ae2B33F52A4",
     inboxId: "ba6c932e48826aafe413d49505531e0c12f5d1c185a2ffe5174c75e9d0692f32",
