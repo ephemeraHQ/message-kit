b51ebb3f911811434bf36d25793bb6f218b7766a
fabri <fguespe@gmail.com>
2024-09-17 16:41:17 -0300

    update

--- examples/group/src/index.ts
+++ examples/group/src/index.ts
@@ -39,7 +39,6 @@ const commandHandlers: CommandHandlers = {
     const intro =
       "Decentralized secure messaging. Built for crypto.\n" +
       "Welcome to the Apps Directory\n\n" +
-      "- 🎰 betbot.eth : Create bets with your friends.\n\n\n" +
       "- 💧 faucetbot.eth : Delivers Faucet funds to devs on Testnet\n\n\n" +
       "- 🛍️ thegeneralstore.eth : Simple ecommerce storefront for hackathon goods\n\n\n" +
       "- 📅 wordlebot.eth : Play daily to the WORDLE game through messaging.\n\n\n" +
