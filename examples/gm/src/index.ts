c92abcfc7357dc33e8ab64805c31d641f0273cf7
fabri <fguespe@gmail.com>
2024-10-26 15:53:37 -0300

    update

--- examples/gm/src/index.ts
+++ examples/gm/src/index.ts
@@ -1,9 +1,12 @@
 import { run, HandlerContext } from "@xmtp/message-kit";
 
-run(async (context: HandlerContext) => {
-  // Get the message and the address from the sender
-  const { content, sender } = context.message;
+run(
+  async (context: HandlerContext) => {
+    // Get the message and the address from the sender
+    const { content, sender } = context.message;
 
-  // To reply, just call `reply` on the HandlerContext
-  await context.send(`gm`);
-});
+    // To reply, just call `reply` on the HandlerContext
+    await context.send(`gm`);
+  },
+  { experimental: true },
+);
