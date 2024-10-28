8f64d98140892b98bc38386c1f50ef1722b2e942
fabri <fguespe@gmail.com>
2024-10-28 15:30:17 -0300

    update

--- examples/gm/src/handler.ts
+++ examples/gm/src/handler.ts
@@ -1,6 +1,5 @@
 import type { HandlerContext } from "@xmtp/message-kit";
 
 export const handler = async (context: HandlerContext) => {
-  console.log("entra handler");
   await context.send("gm");
 };
