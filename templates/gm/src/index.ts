cdc5aa0dca834392709143893e74005bb94faf4d
fabri <fguespe@gmail.com>
2024-11-02 15:49:45 -0300

    update

--- templates/gm/src/index.ts
+++ templates/gm/src/index.ts
@@ -1,9 +1,17 @@
 import { run, HandlerContext } from "@xmtp/message-kit";
 
-run(async (context: HandlerContext) => {
-  // Get the message and the address from the sender
-  const { content, sender } = context.message;
+console.log(process.env.NODE_ENV);
+run(
+  async (context: HandlerContext) => {
+    // Get the message and the address from the sender
+    const { content, sender } = context.message;
 
-  // To reply, just call `reply` on the HandlerContext
-  await context.send(`gm`);
-});
+    // To reply, just call `reply` on the HandlerContext
+    await context.send(`gm`);
+  },
+  {
+    client: {
+      logging: "debug",
+    },
+  },
+);
