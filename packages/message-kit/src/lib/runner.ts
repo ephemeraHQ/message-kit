8d815516c182323fdfe8ca59dbf7d0eb572c6eb7
fabri <fguespe@gmail.com>
2024-11-06 10:29:51 -0300

    export run not as default

--- packages/message-kit/src/lib/runner.ts
+++ packages/message-kit/src/lib/runner.ts
@@ -6,7 +6,7 @@ import { logMessage } from "../helpers/utils.js";
 import { DecodedMessage as DecodedMessageV2 } from "@xmtp/xmtp-js";
 import { streamMessages } from "./streams.js";
 
-export default async function run(handler: Handler, config?: Config) {
+export async function run(handler: Handler, config?: Config) {
   const { client, v2client } = await xmtpClient(config);
   const { inboxId: address } = client;
   const { address: addressV2 } = v2client;
