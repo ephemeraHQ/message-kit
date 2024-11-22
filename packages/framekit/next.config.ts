47723a11c13b5bb9d7a2a181c621e9c6ea5e7e6e
fabri <fguespe@gmail.com>
2024-11-22 16:00:39 -0300

    fabri

--- packages/framekit/next.config.ts
+++ packages/framekit/next.config.ts
@@ -1,14 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  distDir: ".next",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
