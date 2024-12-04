348e3a1421fc8889664434a1e02a2cbb793d4d7a
fabri <fguespe@gmail.com>
2024-12-04 10:43:01 -0300

    frames

--- packages/framekit/src/app/api/chat/route.tsx
+++ packages/framekit/src/app/api/chat/route.tsx
@@ -1,8 +1,8 @@
 import { ImageResponse } from "next/og";
 import { NextRequest } from "next/server";
-import { getUserInfo } from "@xmtp/message-kit";
 import fs from "fs";
 import { join } from "path";
+import { getUserInfo } from "../resolver/route";
 
 const interFontPath = join(process.cwd(), "public/fonts/Inter-Regular.ttf");
 const interFontData = fs.readFileSync(interFontPath);
@@ -15,19 +15,41 @@ const interSemiboldFontData = fs.readFileSync(interSemiboldFontPath);
 
 export async function GET(req: NextRequest) {
   try {
-    const address = req.nextUrl.searchParams.get("address");
-    console.log("address", address);
-    if (!address) {
+    const user = await getUserInfo(
+      req.nextUrl.searchParams.get("address") ?? "",
+    );
+    const params = {
+      url: process.env.NEXT_PUBLIC_URL,
+      ...user,
+    };
+    if (!params.address) {
       return new ImageResponse(
         (
           <div
             style={{
-              color: "white",
-              fontSize: 60,
-              letterSpacing: "-0.025em",
-              lineHeight: 1.4,
+              alignItems: "center",
+              background: "black",
+              display: "flex",
+              flexDirection: "column",
+              flexWrap: "nowrap",
+              height: "100%",
+              justifyContent: "center",
+              textAlign: "center",
+              width: "100%",
             }}>
-            Invalid address
+            <div
+              style={{
+                color: "white",
+                fontSize: 60,
+                fontStyle: "normal",
+                letterSpacing: "-0.025em",
+                lineHeight: 1.4,
+                marginTop: 30,
+                padding: "0 120px",
+                whiteSpace: "pre-wrap",
+              }}>
+              {`Invalid network!`}
+            </div>
           </div>
         ),
         {
@@ -36,7 +58,7 @@ export async function GET(req: NextRequest) {
           fonts: [
             {
               data: interFontData,
-              name: "Inter-Regular",
+              name: "Inter-SemiBold.ttf",
               style: "normal",
               weight: 400,
             },
@@ -44,26 +66,47 @@ export async function GET(req: NextRequest) {
         },
       );
     }
-
-    const userInfo = await getUserInfo(address);
-    const displayName =
-      userInfo?.preferredName ||
-      address.slice(0, 6) + "..." + address.slice(-4);
-
+    // ... existing code ...
     return new ImageResponse(
       (
         <div
           style={{
-            fontSize: "64px",
-            color: "#fa6977",
-            fontFamily: "Inter-SemiBold",
-            textAlign: "center",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            background: "#000000",
+            height: "100%",
+            width: "100%",
+            padding: "48px",
           }}>
-          {displayName}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              justifyContent: "center",
+              gap: "16px",
+            }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "flex-start",
+                gap: "16px",
+                color: "#fa6977",
+              }}>
+              <div style={{ fontSize: "44px" }}>{`Talk to`}</div>
+            </div>
+            <div style={{ fontSize: "64px", color: "#fa6977" }}>
+              {params.preferredName}
+            </div>
+          </div>
         </div>
       ),
       {
-        width: 500,
+        width: 955,
         height: 500,
         fonts: [
           {
---
+++ packages/framekit/src/app/api/resolver/route.ts
@@ -0,0 +1,185 @@
+import { isAddress } from "viem";
+export const converseEndpointURL = "https://converse.xyz/profile/";
+
+export type InfoCache = Map<string, UserInfo>;
+export type ConverseProfile = {
+  address: string | null;
+  onXmtp: boolean;
+  avatar: string | null;
+  formattedName: string | null;
+  name: string | null;
+};
+export type UserInfo = {
+  ensDomain?: string | undefined;
+  address?: string | undefined;
+  preferredName: string | undefined;
+  converseUsername?: string | undefined;
+  ensInfo?: EnsData | undefined;
+  avatar?: string | undefined;
+  converseEndpoint?: string | undefined;
+};
+
+export interface EnsData {
+  address?: string;
+  avatar?: string;
+  avatar_small?: string;
+  converse?: string;
+  avatar_url?: string;
+  contentHash?: string;
+  description?: string;
+  ens?: string;
+  ens_primary?: string;
+  github?: string;
+  resolverAddress?: string;
+  twitter?: string;
+  url?: string;
+  wallets?: {
+    eth?: string;
+  };
+}
+
+let infoCache: InfoCache = new Map();
+
+export const clearInfoCache = (address?: string) => {
+  if (address) {
+    infoCache.delete(address);
+  } else {
+    infoCache.clear();
+  }
+};
+export const getUserInfo = async (
+  key: string,
+  clientAddress?: string,
+): Promise<UserInfo | null> => {
+  let data: UserInfo = {
+    ensDomain: undefined,
+    address: undefined,
+    converseUsername: undefined,
+    ensInfo: undefined,
+    avatar: undefined,
+    converseEndpoint: undefined,
+    preferredName: undefined,
+  };
+  if (typeof key !== "string") {
+    console.error("userinfo key must be a string");
+    return data;
+  }
+  if (infoCache.get(key)) return infoCache.get(key) as UserInfo;
+  key = key?.toLowerCase();
+  clientAddress = clientAddress?.toLowerCase();
+  // Determine user information based on provided key
+  if (isAddress(clientAddress || "")) {
+    data.address = clientAddress;
+  } else if (isAddress(key || "")) {
+    data.address = key;
+  } else if (key.includes(".eth")) {
+    data.ensDomain = key;
+  } else if (["@user", "@me", "@bot"].includes(key)) {
+    data.address = clientAddress;
+    data.ensDomain = key.replace("@", "") + ".eth";
+    data.converseUsername = key.replace("@", "");
+  } else if (key === "@alix") {
+    data.address = "0x3a044b218BaE80E5b9E16609443A192129A67BeA";
+    data.converseUsername = "alix";
+  } else if (key === "@bo") {
+    data.address = "0xbc3246461ab5e1682baE48fa95172CDf0689201a";
+    data.converseUsername = "bo";
+  } else {
+    data.converseUsername = key;
+  }
+
+  data.preferredName = data.ensDomain || data.converseUsername || "Friend";
+  const keyToUse = data.address || data.ensDomain || data.converseUsername;
+
+  if (!keyToUse) {
+    console.log("Unable to determine a valid key for fetching user info.");
+    return data;
+  } else {
+    // Check cache for existing data
+    const cacheData = infoCache.get(keyToUse);
+    if (cacheData) {
+      return cacheData;
+    }
+    // Fetch data based on ENS domain or Converse username
+    if (keyToUse.includes(".eth")) {
+      // Fetch ENS data
+      try {
+        const response = await fetch(`https://ensdata.net/${keyToUse}`);
+        if (!response.ok) {
+          console.error(
+            `ENS data request failed with status or unable to resolve ${keyToUse}`,
+          );
+        } else {
+          const ensData = (await response.json()) as EnsData;
+          if (ensData) {
+            data.ensInfo = ensData;
+            data.ensDomain = ensData.ens || data.ensDomain;
+            data.address = ensData.address || data.address;
+            data.avatar = ensData.avatar_url || data.avatar;
+          }
+        }
+      } catch (error) {
+        //console.error(`Failed to fetch ENS data for ${keyToUse}`);
+      }
+    } else {
+      // Fetch Converse profile data
+      try {
+        const username = keyToUse.replace("@", "");
+        const converseEndpoint = `${converseEndpointURL}${username}`;
+        const response = await fetchWithTimeout(
+          converseEndpoint,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: JSON.stringify({ peer: username }),
+          },
+          5000,
+        );
+        if (!response?.ok) {
+          console.error(
+            `Converse profile request failed with status ${response?.status}`,
+          );
+        }
+        const converseData = (await response?.json()) as ConverseProfile;
+        if (converseData) {
+          data.converseUsername =
+            converseData.formattedName ||
+            converseData.name ||
+            data.converseUsername;
+          data.address = converseData.address || data.address;
+          data.avatar = converseData.avatar || data.avatar;
+          data.converseEndpoint = converseEndpoint;
+        }
+      } catch (error) {
+        console.error("Failed to fetch Converse profile:", error);
+      }
+    }
+
+    data.preferredName = data.ensDomain || data.converseUsername || "Friend";
+    infoCache.set(keyToUse, data);
+    return data;
+  }
+};
+
+const fetchWithTimeout = async (
+  url: string,
+  options: RequestInit,
+  timeout = 5000,
+) => {
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+  try {
+    const response = await fetch(url, {
+      ...options,
+      signal: controller.signal,
+    });
+    clearTimeout(id);
+    return response;
+  } catch (error) {
+    clearTimeout(id);
+    console.error("fetching");
+  }
+};
